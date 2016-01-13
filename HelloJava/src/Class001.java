0213dc20d3b957517a48236da0a1ad6b196a5758
obsicn <obsi@hotmail.com>
2016-01-13 19:27:24 +0800

    add build.xml

---
+++ HelloJava/src/Class001.java
@@ -0,0 +1,9 @@
+
+public class Class001 {
+	
+	public int add(int a, int b)
+	{
+		return a + b;
+	}
+
+}
---
+++ HelloJava/src/Class001Test.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Class001Test {
+
+	@Test
+	public void testAdd() {
+		fail("Not yet implemented");
+	}
+
+}
